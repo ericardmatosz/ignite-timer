0fc695c178da1abd2f94ceb30d36814daa876012
ericardmatosz <ericardmatosz@gmail.com>
2023-09-11 22:48:04 -0300

    feature: create forms logic

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,10 +9,38 @@ import {
   MinutesDurationInput,
 } from './styles'
 
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+import { useForm } from 'react-hook-form'
+
+const newCycleValidationSchema = zod.object({
+  task: zod.string().min(1),
+  durationTime: zod.number().min(5).max(60),
+})
+
 export function Home() {
+  type newCicleFormData = zod.infer<typeof newCycleValidationSchema>
+
+  const { register, handleSubmit, watch, reset } = useForm<newCicleFormData>({
+    resolver: zodResolver(newCycleValidationSchema),
+    defaultValues: {
+      task: '',
+      durationTime: 0,
+    },
+  })
+
+  function handleSubmitForm(data: newCicleFormData) {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const formIsNotValid = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleSubmitForm)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em </label>
           <TaskInput
@@ -20,6 +48,7 @@ export function Home() {
             id="task"
             placeholder="Dê um nome para o seu projeto"
             list="task-suggestion"
+            {...register('task')}
           />
 
           <datalist id="task-suggestion">
@@ -36,6 +65,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('durationTime', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -49,7 +79,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit">
+        <StartCountdownButton disabled={formIsNotValid} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
