127126d6c132fb1374d8bd709885a48cbc3f13a6
ericardmatosz <ericardmatosz@gmail.com>
2023-09-28 23:18:26 -0300

    feature: create a cycle logic

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,13 @@
+import { useContext } from 'react'
 import { HistoryContainer, HistoryList, Status } from './styles'
+import { CyclesContext } from '../../contexts/CyclesContext'
+
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
 export function History() {
+  const { cycles } = useContext(CyclesContext)
+
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
@@ -16,38 +23,33 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Tarefa</td>
-              <td>10 minutos</td>
-              <td>Há dois meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>10 minutos</td>
-              <td>Há dois meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>10 minutos</td>
-              <td>Há dois meses</td>
-              <td>
-                <Status statusColor="yellow">Em andamento</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>10 minutos</td>
-              <td>Há dois meses</td>
-              <td>
-                <Status statusColor="red">Interrompido</Status>
-              </td>
-            </tr>
+            {cycles.map((cycle) => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutes} minutos</td>
+                  <td>
+                    {formatDistanceToNow(new Date(cycle.startDate), {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
+                  <td>
+                    {cycle.finishedDate && (
+                      <Status statusColor="green">Concluído</Status>
+                    )}
+
+                    {cycle.interruptDate && (
+                      <Status statusColor="red">Interrompido</Status>
+                    )}
+
+                    {!cycle.finishedDate && !cycle.interruptDate && (
+                      <Status statusColor="yellow">Em progresso</Status>
+                    )}
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </HistoryList>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,32 @@
-import { Play } from 'phosphor-react'
-import {
-  HomeContainer,
-  FormContainer,
-  CountdownContainer,
-  Separator,
-  StartCountdownButton,
-  TaskInput,
-  MinutesDurationInput,
-} from './styles'
+import { useContext } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { HandPalm, Play } from 'phosphor-react'
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
 
-import { useForm } from 'react-hook-form'
+import { CyclesContext } from '../../contexts/CyclesContext'
 
-const newCycleValidationSchema = zod.object({
-  task: zod.string().min(1),
-  durationTime: zod.number().min(5).max(60),
-})
+import { NewCycleForm } from './components/NewCicleForm'
+import { Countdown } from './components/Countdown'
+import {
+  HomeContainer,
+  StartCountdownButton,
+  StopCountdownButton,
+} from './styles'
 
 export function Home() {
-  type newCicleFormData = zod.infer<typeof newCycleValidationSchema>
+  const { activeCycle, createNewCycle, interruptCurrentCycle } =
+    useContext(CyclesContext)
 
-  const { register, handleSubmit, watch, reset } = useForm<newCicleFormData>({
+  const newCycleValidationSchema = zod.object({
+    task: zod.string().min(1),
+    durationTime: zod.number().min(5).max(60),
+  })
+
+  type newCycleFormData = zod.infer<typeof newCycleValidationSchema>
+
+  const newCycleForm = useForm<newCycleFormData>({
     resolver: zodResolver(newCycleValidationSchema),
     defaultValues: {
       task: '',
@@ -30,59 +34,37 @@ export function Home() {
     },
   })
 
-  function handleSubmitForm(data: newCicleFormData) {
-    console.log(data)
-    reset()
-  }
+  const { handleSubmit, watch, reset } = newCycleForm
 
   const task = watch('task')
   const formIsNotValid = !task
 
+  function handleCreateNewCycle(data: newCycleFormData) {
+    createNewCycle(data)
+
+    reset()
+  }
+
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(handleSubmitForm)} action="">
-        <FormContainer>
-          <label htmlFor="task">Vou trabalhar em </label>
-          <TaskInput
-            type="text"
-            id="task"
-            placeholder="Dê um nome para o seu projeto"
-            list="task-suggestion"
-            {...register('task')}
-          />
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
+        <FormProvider {...newCycleForm}>
+          <NewCycleForm />
+        </FormProvider>
 
-          <datalist id="task-suggestion">
-            <option value="Desenvolver uma aplicação web" />
-            <option value="Desenvolver uma aplicação mobile" />
-            <option value="Desenvolver uma aplicação web e mobile" />
-          </datalist>
+        <Countdown />
 
-          <label htmlFor="durationTime">durante</label>
-          <MinutesDurationInput
-            type="number"
-            id="durationTime"
-            placeholder="00"
-            step={5}
-            min={5}
-            max={60}
-            {...register('durationTime', { valueAsNumber: true })}
-          />
-
-          <span>minutos.</span>
-        </FormContainer>
-
-        <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
-          <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
-        </CountdownContainer>
-
-        <StartCountdownButton disabled={formIsNotValid} type="submit">
-          <Play size={24} />
-          Começar
-        </StartCountdownButton>
+        {activeCycle ? (
+          <StopCountdownButton onClick={interruptCurrentCycle} type="button">
+            <HandPalm size={24} />
+            Interromper
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton disabled={formIsNotValid} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
       </form>
     </HomeContainer>
   )
